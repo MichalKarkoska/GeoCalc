3dfb06118e67a0f3e0cb1e5e58828dd298dbe793
MichalKarkoska <michalkarkoska07@gmail.com>
2026-05-24 17:41:50 +0200

    Fix: Remove duplicate using directives in RoundedPanel.cs

--- Controls/RoundedPanel.cs
+++ Controls/RoundedPanel.cs
@@ -3,10 +3,6 @@ using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
-using System.ComponentModel;
-using System.Drawing;
-using System.Drawing.Drawing2D;
-using System.Windows.Forms;
 
 namespace GeoCalc.Controls
 {
